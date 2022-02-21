c3a5a7823dc20330d942555871bf40dbe6a56810
hollygailthomas <96539803+hollygailthomas@users.noreply.github.com>
2022-02-21 08:26:22 -0800

    Add files via upload

---
+++ helpers.js
@@ -0,0 +1,10 @@
+const convert = (width, height, option) => {
+const options = [160, 320, 480, 640, 900, 1200, 1800, 2400];
+if (options.indexOf(option) == -1) {
+console.log('parameter must be an option');
+} else {
+const max = Math.max(width, height);
+const ratio = option / max;
+return [Math.round(width * ratio), Math.round(height * ratio)];
+}
+}
