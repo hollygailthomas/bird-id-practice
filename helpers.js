2ac840ebc90ad88a8ac1b47963b8ea0bd916cc99
hollygailthomas <96539803+hollygailthomas@users.noreply.github.com>
2022-04-10 14:45:16 -0700

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -8,3 +8,37 @@ const ratio = option / max;
 return [Math.round(width * ratio), Math.round(height * ratio)];
 }
 }
+const history =
+{ array: [],
+	current: undefined,
+	rotateRight: function() {
+		if (this.current) {
+			this.array.push(this.current);
+		}
+		const shifted = this.array.shift();
+		this.current = shifted;
+		return this.current;
+	},
+	rotateLeft: function() {
+		if (this.current) {
+			this.array.unshift(this.current);
+		}
+		const popped = this.array.pop();
+		this.current = popped;
+		return this.current;
+	},
+	add: function (item) {
+    	if (this.current ) {
+      		this.array.push(this.current);
+    	};
+    	this.current = item;
+    },
+	clear: function() {
+		this.current = this.array.pop();
+		populate(this.current);
+	},
+	clearAll: function() {
+		this.array.length = 0;
+		this.clear();
+	}
+};
